4b68c7b22d00c08c8de6c0735e6132054ea23fa1
jayceovaire <118041687+jayceovaire@users.noreply.github.com>
2026-03-23 12:23:12 -0400

    changed fields and views

--- tasks/views.py
+++ tasks/views.py
@@ -13,7 +13,6 @@ from .serializers import TaskSerializer
 def get_tasks(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
-
     return Response(serializer.data)
 
 @api_view(['GET'])
@@ -56,6 +55,7 @@ def create_task(request):
     serializer = TaskSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
