ba0426101e3ba4031ce0b416b24cf22e3782c7dc
jayceovaire <118041687+jayceovaire@users.noreply.github.com>
2026-03-23 12:11:55 -0400

    changed field from date to completed

--- tasks/models.py
+++ tasks/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+from django.utils import timezone
+
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    date = models.DateField()
+    date = models.DateField(default=timezone.now)
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
